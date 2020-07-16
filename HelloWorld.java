367d7a69f062d776836f28cc969ef3ebb34b9c67
Kirill Bykov <kibkibk@gmail.com>
2020-07-16 09:34:24 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,27 @@
+public class HelloWorld {
+public static BigInteger fib(BigInteger n) {
+    if (n.compareTo(BigInteger.ONE) == -1 || n.compareTo(BigInteger.ONE) == 0 ) return n;
+    else 
+        return fib(n.subtract(BigInteger.ONE)).add(fib(n.subtract(BigInteger.ONE).subtract(BigInteger.ONE)));
+}
+
+public static void main(String[] args) {
+    ExecutorService executorService = Executors.newFixedThreadPool(10);
+    for (int j = 0; j < 10; j++) {
+        final int ID = j;
+        executorService.submit(new Runnable() {
+
+            public void run() {
+                for (int i=0;i < Integer.MAX_VALUE; i++) {
+                    System.out.println(ID+" worker: "+i + ": " + fib(new BigInteger(String.valueOf(i))));
+                }
+            }
+        });
+    }        
+}
+}
+
+/** 
 class HelloWorld{public static void main(String args[]) {    
  int n1=0;
  int n2=1;
@@ -16,7 +40,7 @@ class HelloWorld{public static void main(String args[]) {
  }    
   
 }}  
-
+*/
 
 /** 
  * The HelloWorldApp class simply prints "Hello World" to the standard output
